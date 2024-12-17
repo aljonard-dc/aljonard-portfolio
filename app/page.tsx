f0b601f0c04bba16919e1114fd4732c11d726e9a
Aljonard Dela Cruz <aljonarddc@gmail.com>
2024-12-17 22:27:29 +0800

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-blue-300">
+    <div className="bg-blue-200">
       <Hero/>
     </div>
   );
