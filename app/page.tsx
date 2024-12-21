b409bc0053b2e5ba1981e5f5d5e1b5f3f627c54a
Aljonard Dela Cruz <aljonarddc@gmail.com>
2024-12-21 18:37:17 +0800

    chore: move hero and project files to pages folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
+import Hero from "@/app/pages/Hero";
+import Projects from "@/app/pages/Projects";
 
 export default function Home() {
   return (
---
+++ app/pages/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section className='bg-blue-600 w-full h-screen relative'>
+      Project Section
+    </section>
+  )
+}
+
+export default Projects
