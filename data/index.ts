fa03916355bd87ba6f0e082e4879e69a4ec29a24
Aljonard Dela Cruz <aljonarddc@gmail.com>
2025-05-19 22:23:29 +0800

    update data

--- data/index.ts
+++ data/index.ts
@@ -1,100 +1,148 @@
-{/* NavLinks */}
+{
+  /* NavLinks */
+}
 export const navLinks = [
-    {
-        id:1,
-        title:"Home",
-        href:"#home"
-    },
-    {
-        id:2,
-        title:"About",
-        href:"#about"
-    },
-    {
-        id:3,
-        title:"Projects",
-        href:"#projects"
-    },
-    
-    {
-        id:4,
-        title:"Contact",
-        href:"#contact"
-    },
-    
-]
+  {
+    id: 1,
+    title: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    title: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    title: "Projects",
+    href: "#projects",
+  },
+
+  {
+    id: 4,
+    title: "Contact",
+    href: "#contact",
+  },
+];
 
 export const ProjectData = [
-    {
-        id: 1,
-        title: "E-PANDAYALITA",
-        description: "E-PANDAYALITA is an online dictionary dedicated to preserving the traditional language of blacksmithing in the Philippines. It serves as an accessible platform for students, artisans, and enthusiasts to explore terms used in crafting bladed tools and metalworks.",
-        github: "https://github.com/aljonard-dc/epandayalita",
-        link: "https://epandayalita.vercel.app/",
-        images: [
-            "/epandayalita/landingpage.png",
-            "/epandayalita/about.png",
-            "/epandayalita/category.png",
-            "/epandayalita/item.png",
-        ],
-        tags: [
-            { name: "NextJS" },
-            { name: "TypeScript" },
-            { name: "TailwindCSS" },
-            { name: "MongoDB" },
-            { name: "Cloudinary" },
-        ],
-    },
-    {
-        id: 2, 
-        title:"TravelCN",
-        description:"A visually captivating website featuring the top landmarks and tourist spots of Camarines Norte, Philippines. Discover the province’s natural wonders, rich culture, and hidden gems through an engaging guide.",
-        github:"https://github.com/aljonard-dc/travelcn",
-        link:"https://travelcn.vercel.app/",
-        images:["/travelcn/landingpage.png","/travelcn/destinations.png","/travelcn/festivals.png","/travelcn/trends.png","/travelcn/map.png",],
-        tags:[{name:"ReactJS"},{name:"NextJS"},{name:"TypeScript"},{name:"TailwindCSS"}],
-    },
-    {
-        id: 3, 
-        title:"Web-based Reservation Management System",
-        description:"The system offers an intuitive and user-friendly website that provides comprehensive information about the resort, a seamless and efficient reservation management system, and an engaging virtual tour of the resort.",
-        github:"https://github.com/aljonard-dc/Resort-Web-based-Reservation-Management-System",
-        link:"https://naturaverde.website",
-        images:["/naturaverde/landingpage.png","/naturaverde/gallery.png","/naturaverde/takeatour.png","/naturaverde/daytour-reservation.png",],
-        tags:[{name:"HTML"},{name:"CSS"},{name:"Javascript"},{name:"PHP"},{name:"MySQL"},{name:"NodeJS"}],
-    },
-    {
-        id: 4, 
-        title:"St. Martha Management System",
-        description:"The St. Martha Memorial Homes, Inc. Management System manages inventory, scheduling, resource tracking, records of departed individuals, funeral inquiries, payments, and report generation with printable receipts.",
-        github:"https://github.com/aljonard-dc/StMartha-Management-System",
-        images:["/stmartha/contract.png","/stmartha/landingpage.png","/stmartha/plan.png","/stmartha/print.png",],
-        tags:[{name:"Vb.Net"},{name:"MySQL"},{name:"Bunifu Framework"}],
-    },
-    
-]
+  {
+    id: 1,
+    title: "E-PANDAYALITA",
+    description:
+      "E-PANDAYALITA is an online dictionary dedicated to preserving the traditional language of blacksmithing in the Philippines. It serves as an accessible platform for students, artisans, and enthusiasts to explore terms used in crafting bladed tools and metalworks.",
+    github: "https://github.com/aljonard-dc/epandayalita",
+    link: "https://epandayalita.vercel.app/",
+    images: [
+      "/epandayalita/landingpage.png",
+      "/epandayalita/about.png",
+      "/epandayalita/category.png",
+      "/epandayalita/item.png",
+    ],
+    tags: [
+      { name: "NextJS" },
+      { name: "TypeScript" },
+      { name: "TailwindCSS" },
+      { name: "MongoDB" },
+      { name: "Cloudinary" },
+    ],
+  },
+  {
+    id: 2,
+    title: "School Information System",
+    description:
+      "A centralized platform for managing academic records with role-based dashboards, enrollment tracking, subject management, and printable reports for schools.",
+    github: "https://github.com/aljonard-dc/schoolinformationrecordsystem",
+    images: [
+      "/schoolinfo/Admin%20Dashboard.png",
+      "/schoolinfo/Instructor%20Dashboard.png",
+      "/schoolinfo/Student%20Dashboard.png",
+      "/schoolinfo/Admin%20Print%20Enrollment%20List.png",
+      "/schoolinfo/Admin%20Search%20User.png",
+    ],
+    tags: [{ name: "VB.NET" }, { name: "MySQL" }, { name: "Windows Forms" }],
+  },
 
+  {
+    id: 3,
+    title: "TravelCN",
+    description:
+      "A visually captivating website featuring the top landmarks and tourist spots of Camarines Norte, Philippines. Discover the province’s natural wonders, rich culture, and hidden gems through an engaging guide.",
+    github: "https://github.com/aljonard-dc/travelcn",
+    link: "https://travelcn.vercel.app/",
+    images: [
+      "/travelcn/landingpage.png",
+      "/travelcn/destinations.png",
+      "/travelcn/festivals.png",
+      "/travelcn/trends.png",
+      "/travelcn/map.png",
+    ],
+    tags: [
+      { name: "ReactJS" },
+      { name: "NextJS" },
+      { name: "TypeScript" },
+      { name: "TailwindCSS" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Web-based Reservation Management System",
+    description:
+      "The system offers an intuitive and user-friendly website that provides comprehensive information about the resort, a seamless and efficient reservation management system, and an engaging virtual tour of the resort.",
+    github:
+      "https://github.com/aljonard-dc/Resort-Web-based-Reservation-Management-System",
+    link: "https://naturaverde.website",
+    images: [
+      "/naturaverde/landingpage.png",
+      "/naturaverde/gallery.png",
+      "/naturaverde/takeatour.png",
+      "/naturaverde/daytour-reservation.png",
+    ],
+    tags: [
+      { name: "HTML" },
+      { name: "CSS" },
+      { name: "Javascript" },
+      { name: "PHP" },
+      { name: "MySQL" },
+      { name: "NodeJS" },
+    ],
+  },
+  {
+    id: 5,
+    title: "St. Martha Management System",
+    description:
+      "The St. Martha Memorial Homes, Inc. Management System manages inventory, scheduling, resource tracking, records of departed individuals, funeral inquiries, payments, and report generation with printable receipts.",
+    github: "https://github.com/aljonard-dc/StMartha-Management-System",
+    images: [
+      "/stmartha/contract.png",
+      "/stmartha/landingpage.png",
+      "/stmartha/plan.png",
+      "/stmartha/print.png",
+    ],
+    tags: [{ name: "VB.NET" }, { name: "MySQL" }, { name: "Bunifu Framework" }],
+  },
+];
 
 export const ServiceCardContent = [
-    {
-      id: 1,
-      icon:"frontend-icon.svg",
-      title:"Front-End Development",
-      description:"Creating interactive and user-friendly websites using technologies like HTML, Tailwind CSS, ReactJS, and Next.js to deliver an excellent user experience."
-
-    },
-    {
-        id: 2,
-        icon:"responsive-icon.svg",
-        title:"Responsive Design",
-        description:"Ensuring websites look great and function seamlessly across all devices, from desktops to smartphones."
-  
-      },
-      {
-        id: 3,
-        icon:"webdesign-icon.svg",
-        title:"Web Design",
-        description:"Designing modern and visually appealing websites that reflect the project's purpose and personality while ensuring intuitive navigation."
-  
-      },
-  ]
+  {
+    id: 1,
+    icon: "frontend-icon.svg",
+    title: "Front-End Development",
+    description:
+      "Creating interactive and user-friendly websites using technologies like HTML, Tailwind CSS, ReactJS, and Next.js to deliver an excellent user experience.",
+  },
+  {
+    id: 2,
+    icon: "responsive-icon.svg",
+    title: "Responsive Design",
+    description:
+      "Ensuring websites look great and function seamlessly across all devices, from desktops to smartphones.",
+  },
+  {
+    id: 3,
+    icon: "webdesign-icon.svg",
+    title: "Web Design",
+    description:
+      "Designing modern and visually appealing websites that reflect the project's purpose and personality while ensuring intuitive navigation.",
+  },
+];
